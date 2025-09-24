89672ec39decdaba157782e4d41f9307805c61d7
Javedmv <javedmv777@gmail.com>
2025-09-24 12:55:13 +0530

    WIP(body): complete the parseData part of the right side UI && responsive

---
+++ src/Components/ParsedData.tsx
@@ -0,0 +1,83 @@
+import type React from "react";
+
+interface parsedDataProps {
+    aadhaarNumber?: string;
+    name?: string;
+    dob?: string;
+    gender?: string;
+    address?: string;
+    pincode?: string;  
+}
+
+const ParsedData: React.FC<parsedDataProps> = ({
+    aadhaarNumber = "XXXX-XXXX-XXXX",
+    name = "John Doe",
+    dob = "01-01-1990",
+    gender = "Male",
+    address = "123 Street, City, State",
+    pincode = "123456",
+}) => {
+    return(
+        <>
+            <div className="flex flex-col w-full h-full sm:p-4 sm:m-2">
+            {/* Aadhaar Number */}
+            <div className="md:flex md:justify-between">
+                <div className="mb-2 sm:mb-2 md:w-1/2 md:px-2">
+                    <p className="text-xs sm:text-sm font-medium text-gray-600 ">
+                    Aadhaar Number
+                    </p>
+                    <p className="text-sm sm:text-base text-gray-800 border-b border-gray-400 pb-1 truncate">
+                    {aadhaarNumber}
+                    </p>
+                </div>
+
+                {/* Name */}
+                <div className="mb-2 sm:mb-2 md:w-1/2 md:px-2">
+                    <p className="text-xs sm:text-sm font-medium text-gray-600">
+                    Name on Aadhaar
+                    </p>
+                    <p className="text-sm sm:text-base text-gray-800 border-b border-gray-400 pb-1 truncate">
+                    {name}
+                    </p>
+                </div>
+            </div>
+            
+            <div className="md:flex md:justify-between">
+                <div className="mb-2 sm:mb-2 md:w-1/2 md:px-2">
+                    <p className="text-xs sm:text-sm font-medium text-gray-600">
+                    Date of Birth
+                    </p>
+                    <p className="text-sm sm:text-base text-gray-800 border-b border-gray-400 pb-1 truncate">
+                    {dob}
+                    </p>
+                </div>
+
+                <div className="mb-2 sm:mb-3 md:w-1/2 md:px-2">
+                    <p className="text-xs sm:text-sm font-medium text-gray-600">Gender</p>
+                    <p className="text-sm sm:text-base text-gray-800 border-b border-gray-400 pb-1 truncate">
+                    {gender}
+                    </p>
+                </div>
+            </div>
+
+            {/* Address */}
+            <div className="md:px-2 mb-2">
+                <p className="text-xs sm:text-sm font-medium text-gray-600">Address</p>
+                <p className="text-sm sm:text-base text-gray-800 border-b border-gray-400 truncate">
+                {address}
+                </p>
+            </div>
+
+            {/* Pincode */}
+            <div className="md:px-2">
+                <p className="text-xs sm:text-sm font-medium text-gray-600">Pincode</p>
+                <p className="text-sm sm:text-base text-gray-800 border-b border-gray-400 md:w-1/3 truncate">
+                {pincode}
+                </p>
+            </div>
+            </div>
+        </>
+    )
+}
+
+export default ParsedData;
--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,9 +1,10 @@
 
 import ImageField from '../Components/ImageField';
+import ParsedData from '../Components/ParsedData';
 import Button from '../Components/Ui/Button';
 function HomePage() {
   return (
-    <div className="flex flex-col md:flex-row w-full md:my-10">
+    <div className="flex flex-col md:flex-row w-full md:my-13">
         <div className="flex flex-col w-full md:w-1/2">
             <ImageField />
             <div className="flex justify-center items-center">
@@ -16,6 +17,20 @@ function HomePage() {
             </Button>
             </div>
         </div>
+        <div className="flex flex-col w-full md:w-1/2 md:h-100vh">
+            <h2 className="text-base sm:text-lg font-bold underline text-gray-800 sm:pb-2 text-center sm:text-left">
+                Parsed Data
+            </h2>
+            {/* Top Section */}
+            <div className="h-40 md:flex-[3] flex items-center justify-center border border-dashed border-gray-400">
+                <ParsedData/>
+            </div>
+            {/* Bottom Section */}
+            <div className="h-40 md:flex-[4] flex items-center justify-center border border-dashed border-gray-400">
+                <p className="text-gray-600">Dummy Bottom 2/3</p>
+            </div>
+        </div>
+
     </div>
   );
 }
