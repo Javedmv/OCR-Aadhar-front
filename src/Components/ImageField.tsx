881d9c76b581b42af20934d437281fbde140fafd
Javedmv <javedmv777@gmail.com>
2025-09-24 18:31:22 +0530

    feat(body-logic): store input in context

--- src/Components/ImageField.tsx
+++ src/Components/ImageField.tsx
@@ -1,26 +1,55 @@
+import { useContext } from "react";
 import FileUpload from "../Components/Ui/FileUpload";
+import { AadharContext } from "../Context/AadharContext";
+import { toast } from "react-toastify";
 
 const ImageField = () => {
+    const { setAadhar } = useContext(AadharContext);
+
+    function handleFrontImage(ImageFile: File){
+        try {
+            if (!ImageFile) {
+                // TODO: need to clear the preview of the image.
+                throw new Error("Sorry something went wrong. Please reupload the front image.")
+            }   
+            setAadhar({ front: ImageFile });
+        } catch (error:any) {
+            toast.error(error.message || "Something went wrong")
+        }
+    }
+    function handleBackImage(ImageFile: File){
+        try {
+            if (!ImageFile) {
+                throw new Error("Sorry something went wrong. Please reupload the back image.")
+            }  
+            setAadhar({ back: ImageFile });
+        } catch (error:any) {
+            toast.error(error.message || "Something went wrong")
+        }        
+    }
+
     return (
         <>
-                <div className="flex justify-center m-3">
+            <div className="flex justify-center m-3">
                 <FileUpload
                     id="aadharFront"
                     label="Aadhaar Front"
                     onChange={(file) => {
-                        console.log("Front image:", file);
+                        if(!file) return;
+                        handleFrontImage(file)
                     }}
                     />
-                </div>
-                <div className="flex justify-center m-3">
+            </div>
+            <div className="flex justify-center m-3">
                 <FileUpload
                     id="aadharBack"
                     label="Aadhaar Back"
                     onChange={(file) => {
-                        console.log("Back image:", file);
+                        if(!file) return;                        
+                        handleBackImage(file)
                     }}
                     />
-                </div>
+            </div>
         </>
     )
 }
